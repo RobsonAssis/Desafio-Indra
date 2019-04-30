9a92715d1ed3cbece24d369282ea97a200fc0cc7
root <root@localhost.localdomain>
2019-04-30 09:38:06 -0300

    first commit

---
+++ submarino/environment.py
@@ -0,0 +1,32 @@
+\n"""\t\Pyautomators an Framework to Test \n\n\t\t\tsubmarino"""\n\nfrom Pyautomators import fixture
+from Pyautomators.contrib.scenario_autoretry import scenario_retry
+def before_all(context):
+	pass
+
+def before_feature(context,feature):
+	pass
+
+def before_scenario(context,scenario):
+	pass
+
+def before_tag(context,tag):
+	pass
+
+def after_tag(context,tag):
+	pass
+
+def before_step(context,step):
+	pass
+
+def after_step(context,step):
+	pass
+
+def after_scenario(context,scenario):
+	pass
+
+def after_feature(context,feature):
+	pass
+
+def after_all(context):
+	pass
+
