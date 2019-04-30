1cf3dfeb006721ccc7e95da4119a8de472111ea2
root <root@localhost.localdomain>
2019-04-30 10:17:53 -0300

    Guilherme third commit

--- submarino/pages/pages/home.py
+++ submarino/pages/pages/home.py
@@ -5,3 +5,9 @@ class Login:
 
     def go_login(self):
         self.driver.get(self.URL)
+
+    def go_login(self):
+        self.element = self.driver.find_element_by_xpath('/html/body/header/div[2]/div[2]/div[1]/div[3]/span[2]/a/span[1]')
+        self.element.click()
+        self.button = self.driver.find_element_by_xpath('//*[@id="h_usr-signin"]')
+        self.button.click()
