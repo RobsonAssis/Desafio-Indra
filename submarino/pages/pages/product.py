d984ad8bef26a8633c547a9340063bea11999ba7
Marcone lemos <49730516+MaKOXXV@users.noreply.github.com>
2019-05-02 16:54:30 -0300

    Delete product.py

--- submarino/pages/pages/product.py
+++
@@ -1,7 +0,0 @@
-class product:
-    def __init__(self, driver):
-        self.driver = driver
-
-    def buy_product(self):
-        self.button = self.driver.find_element_by_xpath('//*[@id="btn-buy"]')
-        self.button.click()
