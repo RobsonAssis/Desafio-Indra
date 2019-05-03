d112b5562274f61d3687f22b7b47c759c32474e5
Marcone lemos <49730516+MaKOXXV@users.noreply.github.com>
2019-05-02 23:16:16 -0300

    Funciona,porem falha no jenkins

--- submarinoChrome/steps/rm_item_shopping_car.py
+++ submarinoChrome/steps/rm_item_shopping_car.py
@@ -9,7 +9,6 @@ def step_impl(context):
 @then(u'uma solicitação será enviada')
 def step_impl(context):
     #gerando conflito
-    '''context.driver.implicitly_wait(7)
+    context.driver.implicitly_wait(7)
     context.sh_car.reflesh()
-    assert (context.sh_car.text_h2() == 'Seu carrinho está vazio')'''
-    pass
+    assert (context.sh_car.text_h2() == 'Seu carrinho está vazio')
